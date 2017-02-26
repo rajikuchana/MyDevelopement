82ada0c63f751caa2553dcf1a18ad02d793fd079
Rajesh Kuchana <3079800@FutureGroup.Net>
2017-02-26 18:07:24 +0530

    Added New java Cars class file

---
+++ PROJECTS/GitJavaProject/src/com/java/tutorial/Cars.java
@@ -0,0 +1,16 @@
+package com.java.tutorial;
+
+public class Cars {
+
+	public static void main(String[] args) {
+		
+		Cars car= new Cars();
+		car.benzCar();
+
+	}
+	
+	public void benzCar(){
+		System.out.println("This is Benz Car");
+	}
+
+}
--- PROJECTS/GitJavaProject/src/com/java/tutorial/Hello.java
+++ PROJECTS/GitJavaProject/src/com/java/tutorial/Hello.java
@@ -6,7 +6,14 @@ public class Hello {
 		
 		System.out.println("Hello World!");	
 		System.out.println("Second Hello World");
+		
+		Hello world = new Hello();
+		world.helloWorld();
 
+	}
+	
+	public void helloWorld(){
+		System.out.println("Welcome to New World.");
 	}
 
 }
