7a1a04631b20a5e75360c6767961eeba02167209
Rajesh Kuchana <3079800@FutureGroup.Net>
2017-02-26 17:53:37 +0530

    some changes in java files

--- PROJECTS/GitJavaProject/src/com/java/tutorial/Hello.java
+++ PROJECTS/GitJavaProject/src/com/java/tutorial/Hello.java
@@ -2,11 +2,10 @@ package com.java.tutorial;
 
 public class Hello {
 
-
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
-		
+		System.out.println("Hello World!");	
+		System.out.println("Second Hello World");
 
 	}
 
